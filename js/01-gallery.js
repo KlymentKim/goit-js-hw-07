2df2e70e248bb43d12b4d57870e197315e166a57
Klyment <49413026+klim86klim@users.noreply.github.com>
2023-03-23 15:45:52 +0200

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,12 +2,12 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 // Change code below this line
 
-const ul = document.querySelector('.gallery');
-console.log(ul);
-const gallery = galleryItems.reduce((acc, { preview, original, description }) =>
-acc + `<li><img src = "${preview}", "${original}", alt = "${description}"</li>`,'');
+// const ul = document.querySelector('.gallery');
+// console.log(ul);
+// const gallery = galleryItems.reduce((acc, { preview, original, description }) =>
+// acc + `<li><img src = "${preview}", "${original}", alt = "${description}"</li>`,'');
 
- ul.insertAdjacentHTML(`beforeend`, gallery);
+//  ul.insertAdjacentHTML(`beforeend`, gallery);
 
 
 // const instance = basicLightbox.create(
@@ -16,3 +16,56 @@ acc + `<li><img src = "${preview}", "${original}", alt = "${description}"</li>`,
 // ).show();
 
 
+// Створити функцію для рендерингу розмітки на основі масиву даних galleryItems 
+// та шаблону елемента галереї.Функція повинна додати розмітку до елементу з класом gallery на сторінці.
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  }).join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+// Відслідковувати подію кліку на елементі галереї з використанням делегування.Якщо подія відбувається на елементі 
+// з класом gallery__image,
+// необхідно запобігти дії за замовчуванням та отримати URL оригінального зображення з атрибута data - source.
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+
+  const galleryImageEl = event.target;
+  if (galleryImageEl.nodeName !== 'IMG' || !galleryImageEl.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const originalImageUrl = galleryImageEl.dataset.source;
+  openModal(originalImageUrl);
+}
+
+
+// Підключити до HTML-сторінки мініфіковані файли бібліотеки basicLightbox за допомогою CDN-сервісу jsdelivr.
+
+// <!-- Підключення стилів бібліотеки basicLightbox -->
+// <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/basiclightbox/dist/basicLightbox.min.css">
+
+// <!-- Підключення скрипту бібліотеки basicLightbox -->
+// <script src="https://cdn.jsdelivr.net/npm/basiclightbox/dist/basicLightbox.min.js"></script>
+
+// Створити функцію openModal, яка відкриває модальне вікно з оригінальним зображенням.Функція повинна замінити значення атрибута 
+// src у вбудованому зображенні на URL оригінального зображення та відкрит
