db49998df79a17616e86aab0bd6704fb59eb8653
Klyment <49413026+klim86klim@users.noreply.github.com>
2023-03-23 14:30:07 +0200

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
+import * as basicLightbox from 'basiclightbox';
 // Change code below this line
+// const basicLightbox = require('basiclightbox');
+
+const ul = document.querySelectorAll('ul.gallery');
+console.log(ul);
+
+
+
+const instance = basicLightbox.create(
+    // `<img src="assets/images/image.png" width="800" height="600">`)
+    // document.querySelector()
+)
+instance.show()
 
 console.log(galleryItems);
